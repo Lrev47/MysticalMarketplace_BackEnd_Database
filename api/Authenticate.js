369b76ab6aea4ab710b4e52eae66440273c8efde
Lrev47 <luis.revilla47@yahoo.com>
2024-03-15 20:41:41 -0500

    switching to JWT for token

--- api/Authenticate.js
+++ api/Authenticate.js
@@ -1,20 +1,24 @@
+const jwt = require("jsonwebtoken");
+
+const SECRET_KEY = process.env.SECRET_KEY;
+
 function verifyToken(req, res, next) {
   console.log("AUTHTICATING");
   const authHeader = req.headers["authorization"];
   console.log("THIS SHOULD BE THE KEY", authHeader);
   const token = authHeader && authHeader.split(" ")[1];
-  const ProjectToken = "THE_MOST_SECURE_TOKEN_EVER";
 
   if (!token) {
     return res.send({ error: "NO TOKEN" });
   }
-
-  if (token === ProjectToken) {
-    console.log("authenticated");
+  jwt.verify(token, SECRET_KEY, (err, user) => {
+    if (err) {
+      return res.send({ error: "WRONG TOKEN" });
+    }
+    console.log("authenticated", user);
+    req.user = user;
     next();
-  } else {
-    return res.send({ error: "WRONG TOKEN" });
-  }
+  });
 }
 
 module.exports = { verifyToken };
--- api/LoginApi.js
+++ api/LoginApi.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const { LogInUser } = require("../db/user");
+
 const LogInRouter = express.Router();
+
+const SECRET_KEY = process.env.SECRET_KEY;
 
 LogInRouter.post("/", async (req, res, next) => {
   console.log(req.body);
@@ -10,10 +14,14 @@ LogInRouter.post("/", async (req, res, next) => {
     const user = await LogInUser(username, password);
 
     if (user) {
-      const token = "THE_MOST_SECURE_TOKEN_EVER";
+      const payload = {
+        userId: user.id,
+      };
+
+      const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
+
       return res.send({ token, userId: user.id });
     }
-    res.send({});
   } catch (error) {
     console.error(error);
   }
